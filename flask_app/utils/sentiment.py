c8832f63652894c49877c75ab3671d8a71709748
Kevin Peng <kevinpkf@gmail.com>
2022-06-05 22:59:13 -0700

    implemented sentiment summarization

--- flask_app/utils/sentiment.py
+++ flask_app/utils/sentiment.py
@@ -7,3 +7,38 @@ class Sentiment:
     def __getSingleSentiment(self, str):
         return self.analyzer.polarity_scores(str)
     
+    # returns dict of sentiment data in same order of input list and average sentiments
+    def getSentiment(self, strList, threshold):
+        sentiment_list = []
+        compound_sum = 0
+
+        print(f'threshold input: {threshold}')
+
+        summary = {
+            'num_positive': 0,
+            'num_neutral': 0,
+            'num_negative': 0,
+            'average_compound': 0,
+        }
+
+        for str in strList:
+            sentiment = self.__getSingleSentiment(str)
+            sentiment_list.append(sentiment)
+
+            compound = sentiment['compound']
+
+            compound_sum += compound
+
+            if compound >= threshold:
+                summary['num_positive'] += 1
+            elif compound <= -threshold:
+                summary['num_negative'] += 1
+            else:
+                summary['num_neutral'] += 1
+
+        summary['average_compound'] = compound_sum / len(strList)
+        
+        return {
+            'sentiment_list': sentiment_list,
+            'summary': summary,
+        }
